b9db887752acb57ab5bf792379f2d1cacf2b305b
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-03 14:01:07 +0100

    multiplikation

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,7 @@ let subtraktionOperator = 2-1
 console.log(subtraktionOperator)
 console.log("subtraktion: " + subtraktionOperator)
  
-// 
+// Multiplikation
+let multiplikationOperator = 2 * 2
+console.log(multiplikationOperator)
+console.log("multiplication: " + multiplikationOperator) 
