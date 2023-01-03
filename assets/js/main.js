11f218955ec944af0bfe8438af4714fd101e7459
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-03 13:59:06 +0100

    substraktion

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Addition
+let additonOperator = 1+1
+console.log(additonOperator)
+
+console.log("addition: " + additonOperator)
+
+// Subtraktion
+let subtraktionOperator = 2-1
+console.log(subtraktionOperator)
+console.log("subtraktion: " + subtraktionOperator)
+ 
+// 
