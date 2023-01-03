268a058a5bcddd04e80a24cd7217e98381712497
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-03 14:04:07 +0100

    modulus

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,11 @@ console.log(multiplikationOperator)
 console.log("multiplication: " + multiplikationOperator) 
 
 // Division
-let divisionOperator = 4 / 2
-console.log(divisionOperator)
+let divisionOperator = 4 / 2    // 2
+console.log(divisionOperator)   // division: 2
 console.log("diviscion: " + divisionOperator)
+
+// Modul: zeigt an, was der Rest sein würde
+let modulusOperator = 14 % 5 // 4
+console.log(modulusOperator)  // 4
+console.log("modulus: " + modulusOperator) // modulus: 4
